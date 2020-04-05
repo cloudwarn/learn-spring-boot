2025cf51eee3d69077193f2c070fed93d3ad76aa
cloudwarn <zhongnuan2010@163.com>
2020-04-05 11:30:43 +0800

    [新增] 项目初始化

---
+++ learn-spring-security-01/src/main/java/com/nathan/learn/LearnSpringSecurity01Application.java
@@ -0,0 +1,13 @@
+package com.nathan.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringSecurity01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringSecurity01Application.class, args);
+	}
+
+}
---
+++ learn-spring-security-01/src/test/java/com/nathan/learn/LearnSpringSecurity01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathan.learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecurity01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
